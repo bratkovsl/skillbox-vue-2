da32359b87f1bf4018b20c6b88bac4e4aca83154
bratkovsl <bratkovsl@yandex.ru>
2022-06-10 12:10:35 +0300

    Added deploy

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <span class="header__info">Каталог</span>
 
         <a class="header__logo" href="#">
-          <img src="/img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр"
+          <img src="img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр"
                width="190"
                height="33">
         </a>
